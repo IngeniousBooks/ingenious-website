b145a570a9f65091d0991a31a9268a838d1c0af3
alex davis <alexdevdavis@gmail.com>
2023-09-06 16:03:41 +0100

    fix: convert to next Image using viewBox aspect ratio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 
@@ -5,7 +6,7 @@ export default function Header() {
   return (
     <>
     <div className="w-full format h-auto p-4 grid grid-cols-6">
-      < img className="col-span-3" alt="Ingenious Logo" src={"/ingenious-logo.svg"} />
+      < Image width={475.56} height={117.17} priority className="col-span-3" alt="Ingenious Logo" src={"/ingenious-logo.svg"} />
     </div>
     <h2 className="pl-4">Great books</h2>
 
