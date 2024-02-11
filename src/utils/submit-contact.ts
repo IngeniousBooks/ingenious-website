ca86d17d91ea5a395ca5e2b18e4036bb8eea331b
alexdevdavis <alexdevdavis@gmail.com>
2024-02-11 14:41:51 +0000

    fix: edit contact fetch request

--- src/utils/submit-contact.ts
+++ src/utils/submit-contact.ts
@@ -1,22 +1,16 @@
-const submitContact = (contactName: string, email: string, message: string) => {
-  const fetchString = new URL(
-    "https://4276nx0tf2.execute-api.eu-west-2.amazonaws.com/staging/contact"
-  );
-  fetchString.searchParams.append("contactName", contactName);
-  fetchString.searchParams.append("email", email);
-  fetchString.searchParams.append("message", message);
-  fetch(fetchString, {
+const submitContact = async (
+  contactName: string,
+  email: string,
+  message: string
+) => {
+  const fetchString = new URL(process.env.CONTACT_API_STRING as string);
+  const response = await fetch(fetchString, {
     method: "POST",
     mode: "cors",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ contactName, email, message }),
-  })
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      return error;
-    });
+  });
+  return response;
 };
 
 export default submitContact;
