5f5474583ddf987a105069e44d6542adbd712b02
alexdevdavis <alexdevdavis@gmail.com>
2024-02-11 14:46:33 +0000

    fix: add try-catch to contact api call

--- src/utils/submit-contact.ts
+++ src/utils/submit-contact.ts
@@ -4,13 +4,17 @@ const submitContact = async (
   message: string
 ) => {
   const fetchString = new URL(process.env.CONTACT_API_STRING as string);
-  const response = await fetch(fetchString, {
-    method: "POST",
-    mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ contactName, email, message }),
-  });
-  return response;
+  try {
+    const response = await fetch(fetchString, {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contactName, email, message }),
+    });
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default submitContact;
