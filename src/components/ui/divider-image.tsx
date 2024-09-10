86cda0f12fba602ca0096eb869429d5465f61957
alexdevdavis <alexdevdavis@gmail.com>
2024-09-10 14:18:45 +0100

    feat: create divider image component

---
+++ src/components/ui/divider-image.tsx
@@ -0,0 +1,20 @@
+export interface DividerImageProps {
+  imageUrl: string;
+  altText: string;
+}
+
+export default function DividerImage({ imageUrl, altText }: DividerImageProps) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        width: "100%",
+        height: "50vh",
+      }}
+      aria-label={altText}
+    ></div>
+  );
+}
--- src/routes/home-page.tsx
+++ src/routes/home-page.tsx
@@ -4,6 +4,7 @@ import ContactSection from "../components/contact-section/contact-section";
 import SolutionsSection from "../components/solutions-section/solutions-section";
 import ServicesSection from "../components/services-section/services-section";
 import BenefitsSection from "../components/benefits-section/benefits-section";
+import DividerImage from "../components/ui/divider-image";
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
       <HeroSection />
       <SolutionsSection />
       <ServicesSection />
+      <DividerImage
+        imageUrl="public/assets/divider-images/books-scattered-angle-with-spines.jpg"
+        altText="Eleven vibrant books scattered across a white table, spines visible"
+      />
       <BenefitsSection />
       <PartnersSection />
       <ContactSection />
