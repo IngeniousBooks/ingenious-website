ec1503a77e27068df2240544251553b17ae57771
alexdevdavis <alexdevdavis@gmail.com>
2024-09-11 16:03:58 +0100

    refactor: update Bryna's feature testimonial text

--- src/data/feature-testimonial-content.ts
+++ src/data/feature-testimonial-content.ts
@@ -27,17 +27,17 @@ export const featureTestimonialContent: {
     fullName: "Bryna Haynes",
     intro:
       "is the CEO and Founder of World Changers Media, a US-based independent publisher committed to delivering superior-quality transformational nonfiction by, and for, the next generation of thought leaders.",
-    bodyText: `A solopreneur with a **demanding schedule and limited resources**, Bryna required **cost-effective, reliable, and professional services** for managing the pre-press process.
+    bodyText: `A new publisher with a **busy schedule** — and, in her words, 'draconian' standards — Bryna required **cost-effective, reliable, and professional services** for managing the pre-press process.
       \r 
       That’s why she connected with Ingenious, and since 2022, **we’ve collaborated together on over 25 books**.
       \r
-      From unedited manuscript to final print-ready and digital files, we provided book management, copyediting, proofreading, page design, typesetting and eBook services, allowing Bryna **to reclaim her time to operate in her zone of genius and navigate a dynamic future**.`,
+      From raw manuscript to final print-ready and digital files, we provided book management, copyediting, proofreading, page design, typesetting and eBook services, allowing Bryna **to reclaim her time to operate in her zone of genius and serve her clients at the highest level**.`,
     list: {
       intro:
         "With our support, World Changers Media continues to go from strength to strength, achieving:",
       items: [
-        "Multiple industry awards",
-        "Bestselling authors",
+        "An over 70% win rate for authors in major indie book award competitions",
+        "Wall Street Journal and USA Today bestselling authors",
         "Sustained publishing success",
       ],
     },
