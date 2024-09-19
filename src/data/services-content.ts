146bb49d9cce463dc5d3b0a3378391b64b723a07
alexdevdavis <alexdevdavis@gmail.com>
2024-09-19 09:15:27 +0100

    feat: add service summary list to services content

--- src/data/services-content.ts
+++ src/data/services-content.ts
@@ -3,6 +3,7 @@ export interface ServicesContent {
   serviceName: string;
   serviceDescription: string;
   price: string;
+  summaryList: string[];
 }
 
 export const servicesContent: Array<ServicesContent> = [
@@ -12,6 +13,7 @@ export const servicesContent: Array<ServicesContent> = [
     serviceDescription:
       "We work meticulously to ensure your text is error-free, aligns with the author's vision, and resonates with your reading audience, bringing clarity, coherence, and polish to your publications.",
     price: "£600 ($780)",
+    summaryList: ["Line editing", "Copy-editing", "Proofreading", "Indexing"],
   },
   {
     iconURL: "/assets/icons/ingenious-design-icon.svg",
@@ -19,6 +21,12 @@ export const servicesContent: Array<ServicesContent> = [
     serviceDescription:
       "At the heart of our design studio lies a fusion of creativity and deep publishing insight, and we immerse ourselves in the narrative to elevate the visual appeal of your books.",
     price: "£500 ($650)",
+    summaryList: [
+      "Creative direction",
+      "Cover design",
+      "Page layout",
+      "Marketing materials",
+    ],
   },
   {
     iconURL: "/assets/icons/ingenious-project-management-icon.svg",
@@ -26,5 +34,10 @@ export const servicesContent: Array<ServicesContent> = [
     serviceDescription:
       "Our experienced team simplifies the complex to ensure every aspect of the publishing process flows seamlessly.",
     price: "£250 ($350)",
+    summaryList: [
+      "Full project management",
+      "Production management",
+      "Author care",
+    ],
   },
 ];
