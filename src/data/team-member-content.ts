fba8c3dc46152e39bc94d170e2d353889d2a52d3
alexdevdavis <alexdevdavis@gmail.com>
2025-06-04 12:51:11 +0100

    fix: switch staff sequence

--- src/data/team-member-content.ts
+++ src/data/team-member-content.ts
@@ -21,17 +21,6 @@ export const teamProfiles: TeamMemberProfile[] = [
     ],
   },
   {
-    personName: "Alex Davis",
-    jobTitle: "Director of Technology",
-    imageLink: "/assets/staff/ad.png",
-    bioText: [
-      "Hi, I’m Alex, and I look after all things tech at Ingenious.",
-      "In addition to crafting brilliant digital experiences for our readers, I'm here to ensure the systems behind the scenes run smoothly, so we can focus on what's important – making great books.",
-      "An English major and lifelong bibliophile, I'm convinced the printed book is among humankind's greatest ever inventions; we in publishing are honoured to bear custodianship of this remarkable achievement.",
-      "Outside of books and publishing, you’ll find me tinkering with side projects and new tech, out in nature with my family, writing music, or playing Street Fighter.",
-    ],
-  },
-  {
     personName: "James Pople",
     jobTitle: "Creative Director",
     imageLink: "/assets/staff/jp.jpg",
@@ -40,6 +29,17 @@ export const teamProfiles: TeamMemberProfile[] = [
       "I love the variety and creative challenge of my work, as well as the relationships I build with brilliant business owners, agency directors, campaigners, facilitators and strategists along the way.",
       "I'm especially passionate about collaborating with purpose-driven clients — grassroots publishers & global nonprofits — creating work that informs, inspires, and makes a difference.",
       "Outside of publishing I love to hike, travel, sit in a cosy pub with my dog, cook, watch footy, and occasionally do a bit of carpentry.",
+    ],
+  },
+  {
+    personName: "Alex Davis",
+    jobTitle: "Director of Technology",
+    imageLink: "/assets/staff/ad.png",
+    bioText: [
+      "Hi, I’m Alex, and I look after all things tech at Ingenious.",
+      "In addition to crafting brilliant digital experiences for our readers, I'm here to ensure the systems behind the scenes run smoothly, so we can focus on what's important – making great books.",
+      "An English major and lifelong bibliophile, I'm convinced the printed book is among humankind's greatest ever inventions; we in publishing are honoured to bear custodianship of this remarkable achievement.",
+      "Outside of books and publishing, you’ll find me tinkering with side projects and new tech, out in nature with my family, writing music, or playing Street Fighter.",
     ],
   },
 ];
